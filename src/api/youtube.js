98fe0ea50e2575e2797bb6fa2914d0c2c7b83148
DA-coder-ethio <64494269+DA-coder-ethio@users.noreply.github.com>
2020-12-11 15:30:45 +0200

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBiwBf7n91rVmr__Up4IM1TagbQSsNZNgA";
+const KEY = "";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
